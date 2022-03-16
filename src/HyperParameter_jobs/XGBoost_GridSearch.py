2c2ffa472da962afa93d566da5f82a930367004b
AzaelCC <azaelc2@gmail.com>
2022-03-16 14:13:41 -0600

    Import error

--- src/HyperParameter_jobs/XGBoost_GridSearch.py
+++ src/HyperParameter_jobs/XGBoost_GridSearch.py
@@ -25,6 +25,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
 
 from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold
 from xgboost import XGBClassifier
