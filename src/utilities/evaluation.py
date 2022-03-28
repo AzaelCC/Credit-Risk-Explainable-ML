3d4f1e6a50b361071f9ca240dd8a61db9ea40bc4
AzaelCC <azaelc2@gmail.com>
2022-03-28 00:02:38 -0600

    Change evaluation settings

--- src/utilities/evaluation.py
+++ src/utilities/evaluation.py
@@ -1,11 +1,14 @@
 # spot check machine learning algorithms on the german credit dataset
-from sklearn.metrics import fbeta_score, roc_auc_score, make_scorer, balanced_accuracy_score, classification_report
+from sklearn.metrics import fbeta_score, roc_auc_score, make_scorer, balanced_accuracy_score, classification_report, f1_score
 from sklearn.model_selection import cross_val_score, RepeatedStratifiedKFold
 from numpy import min, max, mean
 
 # calculate f2-measure
 def f2_measure(y_true, y_pred):
     return fbeta_score(y_true, y_pred, beta=2)
+
+def f1_macro(y_true, y_pred):
+    return f1_score(y_true, y_pred, average='macro')
  
 # evaluate a model
 def evaluate_model(X, y, model, metric=f2_measure, splits=5, reps=3, seed=None):
